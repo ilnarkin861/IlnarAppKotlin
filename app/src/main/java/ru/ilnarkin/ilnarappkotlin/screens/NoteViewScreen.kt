5c2e842b5db798792e4e0fc3876abf9b056acf26
ilnar861 <ilnarkino@yandex.ru>
2025-06-25 17:06:51 +0300

    Removed unused imports

--- app/src/main/java/ru/ilnarkin/ilnarappkotlin/screens/NoteViewScreen.kt
+++ app/src/main/java/ru/ilnarkin/ilnarappkotlin/screens/NoteViewScreen.kt
@@ -1,8 +1,7 @@
 package ru.ilnarkin.ilnarappkotlin.screens
 
-import androidx.compose.foundation.Image
+
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
