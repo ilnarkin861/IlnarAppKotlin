1c252da62aef9fba812e970d70f20d3d5482de62
ilnar861 <ilnarkino@yandex.ru>
2025-07-02 16:30:59 +0300

    Added @RequiresApi

--- app/src/main/java/ru/ilnarkin/ilnarappkotlin/MainActivity.kt
+++ app/src/main/java/ru/ilnarkin/ilnarappkotlin/MainActivity.kt
@@ -1,9 +1,11 @@
 package ru.ilnarkin.ilnarappkotlin
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.displayCutoutPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -27,6 +29,7 @@ import ru.ilnarkin.ilnarappkotlin.ui.theme.IlnarAppKotlinTheme
 
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -40,6 +43,7 @@ class MainActivity : ComponentActivity() {
 
 
 
+@RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
